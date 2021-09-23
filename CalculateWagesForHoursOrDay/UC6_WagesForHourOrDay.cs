fdeb71d1ce35551e831ebc4ad56739fed5d92956
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 13:14:33 +0530

    Calculate Wages For Hours or Days

---
+++ CalculateWagesForHoursOrDay/UC6_WagesForHourOrDay.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CalculateWagesForHoursOrDay
+{
+    class UC6_WagesForHourOrDay
+    {
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
+        public const int MAX_HOURS_IN_MONTH = 100;
+        static void Main(string[] args)
+        {
+            //Variables
+            int EmpHrs = 0;
+            int TotalEmpHours = 0;
+            int TotalEmpWages = 0;
+            int TotalWorkingDays = 0;
+
+            while(TotalEmpHours <= MAX_HOURS_IN_MONTH && TotalWorkingDays < NUMBER_OF_WORKING_DAYS)
+            {
+                TotalWorkingDays++;
+                Random random = new Random();
+                int CheckEmp = random.Next(0, 3);
+
+                //Using Switch Case
+                switch (CheckEmp)
+                {
+                    case IS_PART_TIME:
+                        EmpHrs = 4;
+                        break;
+
+                    case IS_FULL_TIME:
+                        EmpHrs = 8;
+                        break;
+
+                    default:
+                        EmpHrs = 0;
+                        break;
+                }
+
+                TotalEmpHours += EmpHrs;
+                Console.WriteLine("Days: " + TotalWorkingDays + " Employee Hours: " + EmpHrs);
+            }
+
+            TotalEmpWages = TotalEmpHours * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wages in Month : " + TotalEmpWages);
+        }
+    }
+}
